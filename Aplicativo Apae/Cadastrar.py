9ae34e0dc8eda4e1ff600c7aca98e58661c0bcff
IagoCF <iagofreits@gmail.com>
2024-05-24 20:20:51 -0300

    Start
    Criação da Interface principal

---
+++ Aplicativo Apae/Cadastrar.py
@@ -0,0 +1,12 @@
+from customtkinter import *
+
+def pop():
+    cadastrarInterface = CTk()
+    cadastrarInterface.title("Cadastrar cliente")
+    cadastrarInterface.geometry("400x300+725+250")
+
+    
+
+
+
+    cadastrarInterface.mainloop()
---
+++ Aplicativo Apae/Interface.py
@@ -0,0 +1,43 @@
+from customtkinter import *
+import Cadastrar
+
+app = CTk()
+app.title("APAE")
+app.geometry("600x400+600+200")
+
+tab = CTkTabview(master=app)
+tab.pack(expand=1, fill="both", padx = 10, pady = 10)
+
+#Event Handling
+def cadastrar_contribuinte():
+    Cadastrar.pop()
+
+tab.add("Arquivo")
+#
+
+tab.add("Contribuintes")
+#
+cadastrar = CTkButton(master=tab.tab("Contribuintes"), text="Cadastrar", command=cadastrar_contribuinte)
+cadastrar.place(relx=0.5, rely=0.2, anchor = "center")
+
+alterar = CTkButton(master=tab.tab("Contribuintes"), text="Alterar")
+alterar.place(relx=0.5, rely=0.3, anchor = "center")
+
+exlcuir = CTkButton(master=tab.tab("Contribuintes"), text="Excluir")
+exlcuir.place(relx=0.5, rely=0.4, anchor = "center")
+
+consultar = CTkButton(master=tab.tab("Contribuintes"), text="Consultar Todos")
+consultar.place(relx=0.5, rely=0.6, anchor = "center")
+
+tab.add("Recibos")
+#
+
+tab.add("Relatórios")
+#
+
+tab.add("Sobre")
+#
+
+
+app.mainloop()
+
